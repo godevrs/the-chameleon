8c1c001cc69a8c02f6c705e9f07df4d0eead2b21
Goran Petrovic <petrovicg@gmail.com>
2016-10-25 05:43:13 +0200

    no message

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 	global 	$TheChameleon;
 	global 	$TheChameleonOption;
 	$TheChameleon->Head->view();
-	$TheChameleon->Upper->template('upper');
+	$TheChameleon->Upper->template('upper');	
 	$TheChameleon->Header->template($TheChameleonOption['primary_menu_type']);  
 	$TheChameleon->Top->template('top');
 	
